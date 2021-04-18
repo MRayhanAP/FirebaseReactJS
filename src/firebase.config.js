0ffff09415921577a2f10ce111d27f854990de8d
MRayhanAP <nxy929@gmail.com>
2021-04-18 23:52:14 +0700

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,8 @@
+  export default {
+    apiKey: "AIzaSyCSy3qq67k94JSlBy-KHHkUIIQ30bgr2k4",
+    authDomain: "fir-react-5aff3.firebaseapp.com",
+    projectId: "fir-react-5aff3",
+    storageBucket: "fir-react-5aff3.appspot.com",
+    messagingSenderId: "958027680181",
+    appId: "1:958027680181:web:ca549037ef84c5968e31fc"
+  };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import "./style.css";
+import reportWebVitals from "./reportWebVitals";
+import Header from "./Header";
+import routes from "./routes";
+import firebase from "firebase";
+import firebaseConfig from "./firebase.config";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+firebase.initializeApp(firebaseConfig);
+
+export const AuthContext = React.createContext(null);
+
+function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+      Is logged in? {JSON.stringify(isLoggedIn)}
+      <div className="App">
+        <Router>
+          <Header />
+
+          <Switch>
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                exact={route.exact}
+                component={route.main}
+              />
+            ))}
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
